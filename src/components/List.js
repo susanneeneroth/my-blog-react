4b7ab81a1d8068dd33882070ee2e4eb5c3654c09
susanneeneroth <susanne@centimedia.se>
2021-04-01 17:46:36 +0200

    Fix of css and added some classes to List.js

--- src/components/List.js
+++ src/components/List.js
@@ -4,17 +4,17 @@ import { Link } from 'react-router-dom'
 const List = ({blogs, title, handleDelete}) => {
     return ( 
         <div className="blog-list" key={blogs.id}>
-            <h2>{ title }</h2>
-            {blogs.map((blogs) => (
-                <div className="blog-preview" key={blogs.id}>
-                <Link to={`/blogs/${blogs.id}`}>
+        <h2>{ title }</h2>
+           {blogs.map((blogs) => (
+               <div className="blog-preview" key={blogs.id}>
+                  <Link to={`/blogs/${blogs.id}`}>
                     <h2>{blogs.title}</h2>
-                    <p>{blogs.body}</p>
-                    <p>{blogs.author}</p>
-                </Link>
-                </div>
-            )
-            )}
+                      <p>{blogs.body}</p>
+                      <p className="blog-author">Written by {blogs.author}</p>
+                  </Link>
+                  
+               </div>
+           ))}
         </div>
      );
 }
--- src/useFetch.js
+++ src/useFetch.js
@@ -2,39 +2,39 @@ import { useState, useEffect } from 'react'
 
 const useFetch = (url) => {
     const [data, setData] = useState(null);
-    const [isPendning, setIsPending] = useState(true);
+    const [isPending, setIsPending] = useState(true);
     const [error, setError] = useState(null);
 
     useEffect(() => {
-        const abortCont = new AbortController();
+      const abortCont = new AbortController();
 
         setTimeout(() => {
-            fetch(url, { signal: abortCont.signal })
-            .then(res => {
-                if(!res.ok) {
-                    throw Error ('Could not fetch the data');
-                }
-                return res.json()
-            })
-            .then((data) => {
-                setData(data)
-                setIsPending(false);
-                setError(null);
-            })
-            .catch(err => {
-                if (err.name === 'AbortError') {
-                    console.log('fetch aborted')
-                } else {
-                    setIsPending(false)
-                    setError(err.message);
-                }
-            })
+           fetch(url, { signal: abortCont.signal })
+           .then(res => {
+               if(!res.ok) {
+                 throw Error ('Could not fetch the data');
+               }
+             return res.json()
+           })
+           .then((data) => {
+             setData(data)
+             setIsPending(false);
+             setError(null);
+           })
+           .catch(err => {
+             if (err.name === 'AbortError') {
+               console.log('fetch aborted')
+             } else {
+              setIsPending(false);
+              setError(err.message);
+             }
+           })
         }, 1000);
 
         return () => abortCont.abort();
-    }, [url]);
+  }, [url]);    
 
-    return { data, isPendning, error}
+  return { data, isPending, error }
 }
- 
+
 export default useFetch;
