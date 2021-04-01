ddecc7d3cf1c5d8236b008e3254c3e3b65a07550
susanneeneroth <susanne@centimedia.se>
2021-04-01 19:26:54 +0200

    Fix of function delete blog

--- src/components/Details.js
+++ src/components/Details.js
@@ -3,11 +3,11 @@ import useFetch from '../useFetch';
 
 const Details = () => {
     const { id } = useParams();
-    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs' + id);
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
     const history = useHistory();
 
     const handleClick = () => {
-        fetch('http://localhost:8000/blogs' + blog.id, {
+        fetch('http://localhost:8000/blogs/' + blog.id, {
             method: 'DELETE'
         }).then(() => {
             history.push('/');
